44675ec6f5617493d12cb55d088b50ffb54bb5f0
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-28 22:28:36 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,21 @@
+public class str
+{   public static void main(String args[]){                                                 
+        String s= new String();        
+        System.out.println("Answer:"+s);
+         char ch[]={'a','b','c'};       
+        String k =new String(ch);    
+          System.out.println("Answer:"+k);          
+          char c[]={'a','b','c','d','e','f'};         
+          String p= new String(c,2,3);         
+            System.out.println("Answer:"+p);            
+            char f[]={'j','a','y','h'};
+            String z=new String(f);
+            String z1=new String(z);
+              System.out.println("Answer:"+z1);    
+              String m="hello";
+                System.out.println("Answer:"+m);
+                    String q=new String("hi");
+          
+            System.out.println("Answer:"+q);
+    }    
+}
