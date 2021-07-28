cce1e19dae583fdbbd317d20442efc60b4adb4b5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-28 22:30:20 +0530

    Create string_methods.java

---
+++ string_methods.java
@@ -0,0 +1,42 @@
+public class string_methods
+{
+    public static void main(String args[]){
+  String s1="Football";
+  String s2="Shootball";
+  if((s1.compareTo(s2))==0)
+  System.out.println("The two strings are equal");
+  else if((s1.compareTo(s2))<0)
+  System.out.println(s1+"......"+s2);
+  else
+  System.out.println(s2+"......"+s1);  
+  String s3=("Now is the time to be happy for all the good people");
+  System.out.println("indexOf('t'):"+s3.indexOf('t'));
+  System.out.println("indexOf('the'):"+s3.indexOf("the"));  
+  String s4=("Now is the time to be happy for all the good people");
+  System.out.println("lastIndexOf('t'):" +s4.lastIndexOf('t'));
+  System.out.println("lastIndexOf('the'):"+s4.lastIndexOf("the"));  
+  String s5="Now is the time to be happy for all the good people";
+  System.out.println("Length:"+s5.length());
+  String s6="Hello";
+  System.out.println("Replaced String:" +s6.replace('H','Y')); 
+  String s7="Now is the time to be happy for all the good people"; 
+  System.out.println(s7.substring(7));
+  System.out.println(s7.substring(7,27));  
+  String s8="Hello";
+  System.out.println(s8.toLowerCase());
+  System.out.println(s8.toUpperCase()); 
+  String s9=" Hello class ";
+  System.out.println(s9.trim());  
+  System.out.println("isLowerCase:"+Character.isLowerCase('D'));
+  System.out.println("isLowerCase:"+Character.isLowerCase('f'));  
+  System.out.println("isUpperCase:"+Character.isUpperCase('D'));
+  System.out.println("isUpperCase:"+Character.isUpperCase('f'));  
+  System.out.println("isLetter:"+Character.isLetter('c'));
+  System.out.println("isLetter:"+Character.isLetter('5')); 
+  System.out.println("isDigit:"+Character.isDigit('c'));
+  System.out.println("isDigit:"+Character.isDigit('5'));  
+  System.out.println("isLetterOrDigit():"+Character.isLetterOrDigit(0*0041));
+  System.out.println("isLetterOrDigit():"+Character.isLetterOrDigit(0*0036));
+  System.out.println("isLetterOrDigit():"+Character.isLetterOrDigit(0*0024));  
+}
+}
