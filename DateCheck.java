33758c9c81e8ad627e73a0c7bd84f166f64e4dae
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-28 19:50:57 +0530

    Create DateCheck.java

---
+++ DateCheck.java
@@ -0,0 +1,48 @@
+import java.io.*;
+class date
+{
+public static void main(String args[]) throws IOException 
+{
+int dd, mm, yy;
+int d1, d2, q, r, i, j, num, num1;
+int[] X = new int[6];
+DAtaInputStream in = new DataInputStream(System.in);
+System.out.print("Enter date : ");
+dd = Integer.parseInt(in.readLine());
+System.out.print("Enter month : ");
+mm = Integer.parseInt(in.readLine());
+System.out.print("Enter year : ");
+yy = Integer.parseInt(in.readLine());
+if ( mm==1 || mm == 3 || mm == 5 || mm == 7 || mm == 10 || mm == 12)
+{
+if (dd<=31 && dd >0)
+System.out.println("Valid date ");
+else
+System.out.println("Invalid date ");
+}
+else
+if ( mm==4 || mm == 6 || mm == 9 || mm == 11)
+{
+if (dd<=30 && dd >0)
+System.out.println("Valid date ");
+else
+System.out.println("Invalid date ");
+}
+else
+if (mm == 2)
+{
+if (( yy % 100 == 0) && (yy%400 == 0) || (yy%100 == 0) && (yy%4 == 0))
+if (dd>0 && dd<=29)
+System.out.println("Valid date ");
+else
+System.out.println("Invalid date ");
+else
+if (dd>0 && dd<= 28)
+System.out.println("Valid date ");
+else
+System.out.println("Invalid date ");
+}
+else
+System.out.println("Invalid date ");
+}
+}
