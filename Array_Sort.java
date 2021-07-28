fce1cfd1735a1b32f9d1537ebe291d8cbcc6e8dc
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-28 21:54:25 +0530

    Create Array_Sort.java

---
+++ Array_Sort.java
@@ -0,0 +1,54 @@
+import java.io.*;
+public class Array_Sort
+{
+    public static void main(String args[])throws IOException {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Enter row size");
+       int r=Integer.parseInt(br.readLine());
+       System.out.println("Enter column size");
+       int c=Integer.parseInt(br.readLine());
+       int a1[][]=new int[r][c];
+       System.out.println("Enter values for array");
+       for(int i=0;i<r;i++){
+           for(int j=0;j<c;j++){
+               a1[i][j]=Integer.parseInt(br.readLine());
+            }
+        } 
+        int size=r*c;
+        int arr[]=new int [size];
+        int pos=0;
+        for(int i=0;i<r;i++){
+           for(int j=0;j<c;j++){
+               arr[pos]= a1[i][j];
+               pos++;
+            }
+        } 
+       for (int i=0;i<size;i++){
+           for(int j=0;j<size;j++){
+           if (arr[i]<arr[j]){
+               int temp=arr[i];
+               arr[i]=arr[j];
+               arr[j]=temp;
+            }
+        }
+    }
+    int large = arr[size-1];
+    int seclarge=arr[size-2];
+           pos=0;
+        for(int i=0;i<r;i++){
+           for(int j=0;j<c;j++){
+               a1[i][j]=arr[pos];
+               pos++;
+            }
+        } 
+        System.out.println("The resultant array is:");
+        for(int i=0;i<r;i++){
+           for(int j=0;j<c;j++){
+               System.out.print(a1[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println("Largest element:\t"+large);
+                System.out.println("Second Largest element:\t"+seclarge);
+            }
+    }
