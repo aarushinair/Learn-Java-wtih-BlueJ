dd64762264678dc5f9032c8af9e13379ff3be992
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 21:33:05 +0530

    Update seriesum.java

--- seriesum.java
+++ seriesum.java
@@ -64,3 +64,18 @@ ob.display();//calling display()
  * sum          double  stores the series sum
  * t            double  stores the value term of the series
  */
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
