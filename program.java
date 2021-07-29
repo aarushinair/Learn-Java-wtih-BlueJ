5d770e3b9256f9feb2f8a6fa303df2b83fa6b7cd
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 21:31:44 +0530

    Update program.java

--- program.java
+++ program.java
@@ -26,3 +26,18 @@ for(int m=0;m<l;m++){
 }
 }
 }
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
