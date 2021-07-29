e166097332110c0ccc3805ff3f33ad2265ee8ec5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 21:36:06 +0530

    Update vowels_replace.java

--- vowels_replace.java
+++ vowels_replace.java
@@ -38,3 +38,18 @@ for(int y=0;y<a.length();y++){//Initiating for loop
  *  z                   char            to store the character value of an integer value
  *  y                   int             to store the integer value of the array index
  */
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
