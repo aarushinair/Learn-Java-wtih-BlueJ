f20cfc2697d3d501e023ff37dd8757ea7dbb5ce5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 21:14:41 +0530

    Update Perimeter2.java

--- Perimeter2.java
+++ Perimeter2.java
@@ -42,3 +42,18 @@ public class Area extends Perimeter
         ob.show();
     }
     }
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
