10ebfca2e2ea12844eb9536d7b0aa49b921bcf6f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 21:34:38 +0530

    Update stringmethods.java

--- stringmethods.java
+++ stringmethods.java
@@ -25,3 +25,18 @@ class stringmethods{
         System.out.println(s9.equalsIgnoreCase(s10));   
     }
 }
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
