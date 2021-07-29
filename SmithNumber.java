1e339e281180bfb4c0a9415dcb4da9dff935edc0
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 21:23:54 +0530

    Update SmithNumber.java

--- SmithNumber.java
+++ SmithNumber.java
@@ -62,3 +62,18 @@ public static void main(String args[]) throws IOException
  * a                        int                 to store return value of sum of digits
  * b                        int                 to store return value of prime factors
  */
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
