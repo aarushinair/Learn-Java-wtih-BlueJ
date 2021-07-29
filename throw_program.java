d7ccf907ca7b636e2591add4b45262c48068b7b0
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 21:35:22 +0530

    Update throw_program.java

--- throw_program.java
+++ throw_program.java
@@ -36,3 +36,18 @@ public class throw_program
         return max;
         }             
     }
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
