87b90b8184e5db31e96d3f6efeac2c6fff9e85ee
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-10-02 17:27:26 +0530

    Update LL_sort.java

--- LL_sort.java
+++ LL_sort.java
@@ -1,51 +1,70 @@
 import java.io.*;
-public class LL_sort
+class LL_sort
 {
-  Node start,end;
-  int size,flag;
-  LL_sort(){
-      start=null;
-      end=null;
-      size=0;
+   Node start, end;
+   int size;
+   LL_sort(){
+       start=null;
+       end=null;
+       size=0;
     }
-    void insert(int e){
-        Node nptr=new Node(e,null);
-        Node ptr=start;
-        LinkedList ll=new LinkedList ();
-        int m=0,flag=0;
-        if (ptr==null){
-            ll.insertAtStart(e);
+   void insert(int d){
+       Node nptr=new Node (d,null);
+        if (start==null){
+            start=nptr;
+            end=nptr;
             size++;
-           
-        }
-        for (int i=0;i<=size;i++){
-                    int data=ptr.getData();
-            if (e<data){
-            flag=0;
-            m=i;
-            break;
         }
         else{
-            flag=1;
-          }
-        ptr=ptr.getLink();
-     }
-        if (flag==0){
-            ll.insertAtPos(e,m);
-        }
-        else{
-            ll.insertAtEnd(e);
+           end.setLink(nptr);
+           end=nptr;
+           size++;
         }
   }
-  public static void main (String args[])throws IOException{
-      BufferedReader br=new BufferedReader (new InputStreamReader (System.in));
-      LL_sort ob=new LL_sort();
-      System.out.println("Enter element");
-      int ele=Integer.parseInt(br.readLine());
-      ob.insert(ele);
+  public void linearSearch(int item){
+    Node ptr = start ;
+    int i ;
+    boolean flag = false;
+    for(i = 0 ; i < size ; i ++){
+        if(item == ptr.getData()){
+            flag = true ;
+            break ;
+        }
+        ptr=ptr.getLink();
     }
-      
+    if(flag == true)
+        System.out.println("Element located at position : " + i) ;
+    else
+        System.out.println("Element does not exist") ;
     }
+  void display(){
+      Node ptr=start;             
+       while(ptr!=null)
+       {
+             System.out.println(ptr.getData());
+             ptr=ptr.getLink();   
+            }   
+  }
+ public static void main (String args[]) throws IOException{
+    LL_sort l1 = new LL_sort();
+    int choice=1;
+    BufferedReader br=new BufferedReader (new InputStreamReader (System.in));
+     while(choice !=0){
+      System.out.println("Enter element for list elements");
+     int d =Integer.parseInt(br.readLine());
+    l1.insert(d);
+    System.out.println("To continue press 1 or else press 0");
+    choice=Integer.parseInt(br.readLine());    
+   }
+   System.out.println("Enter element to search");
+   int item =Integer.parseInt(br.readLine());
+   System.out.println("The list being searched is:");
+   l1.display();
+   l1.linearSearch(item);
+  }
+ }
+
+ 
 
 
 
