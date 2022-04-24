05b9e064a1e6d6ae7d7c891b979c7bd6605c5129
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-04-24 17:36:24 +0530

    Update PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -29,9 +29,6 @@ public class PerfectNumberProgram {
 
 
 
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
