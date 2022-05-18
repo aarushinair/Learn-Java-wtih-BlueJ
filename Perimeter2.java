621efe734d7dd8088ea7f810453ee9f0c1dde69e
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-05-18 23:21:42 +0530

    Update Perimeter2.java

--- Perimeter2.java
+++ Perimeter2.java
@@ -46,10 +46,6 @@ public class Area extends Perimeter
 
 
 
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
