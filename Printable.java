edcc96683d2e3faa2dd36436e40b585b648575f5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-08-24 11:02:31 +0530

    Update Printable.java

--- Printable.java
+++ Printable.java
@@ -23,10 +23,6 @@ public class test implements Printable
 
 
 
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
