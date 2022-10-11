809f43f32c430b5f8baa2f4331b945151f8c8a80
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-10-12 01:11:24 +0530

    Update acceptscanner.java

--- acceptscanner.java
+++ acceptscanner.java
@@ -20,10 +20,6 @@ public class acceptscanner
 
 
 
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
