54c24e1b3e695ba2cd9d5549bea3a559d0ed896f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-12-07 11:15:51 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -29,10 +29,6 @@ public class checkPrime {
 
 
 
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
