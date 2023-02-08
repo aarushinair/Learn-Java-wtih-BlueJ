f5c4a933ad02892e41757b0b6ad459d87678d670
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-02-08 11:25:06 +0530

    Update stack.java

--- stack.java
+++ stack.java
@@ -88,7 +88,7 @@ public static void main (String args[]) {//start of main metho
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -96,3 +96,4 @@ public static void main (String args[]) {//start of main metho
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
