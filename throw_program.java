b50668e29b141e26bf76be0b3cafa56853b3e026
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-08-02 06:55:54 +0530

    Update throw_program.java

--- throw_program.java
+++ throw_program.java
@@ -43,7 +43,7 @@ public class throw_program
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -51,3 +51,4 @@ public class throw_program
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
