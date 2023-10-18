f8a7ab0e27c46b106f559f605eca6361c8b6e6c3
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-10-18 08:05:00 +0530

    Update AIE.java

--- AIE.java
+++ AIE.java
@@ -51,7 +51,7 @@ public class AIE
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -59,3 +59,4 @@ public class AIE
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
