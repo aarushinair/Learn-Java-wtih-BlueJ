46d477bf6490da75b8a521e3f85b6c1814ca1f90
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-12-30 00:14:40 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -64,7 +64,7 @@ public class Account extends Bank
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -72,3 +72,4 @@ public class Account extends Bank
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
