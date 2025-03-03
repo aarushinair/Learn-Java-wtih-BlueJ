830a209f85860710511572c30572fcee8491b522
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-03-03 18:03:05 +0530

    Update Frequency.java

--- Frequency.java
+++ Frequency.java
@@ -62,7 +62,7 @@ public class Frequency
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -70,3 +70,4 @@ public class Frequency
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
