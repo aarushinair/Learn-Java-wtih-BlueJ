a96d2067e2ee08e5b2b1499a924492feb39f66ec
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-03-16 01:03:26 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -220,7 +220,7 @@ public class LinkedList
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -228,3 +228,4 @@ public class LinkedList
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
