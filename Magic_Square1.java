98b865a61412985edb53836c8d875a59d688e31d
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-03-18 08:44:59 +0530

    Update Magic_Square1.java

--- Magic_Square1.java
+++ Magic_Square1.java
@@ -83,7 +83,7 @@ public class magic_square1{
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -91,3 +91,4 @@ public class magic_square1{
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
