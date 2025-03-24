1553959cd215ea5de525cde47173c15b279ac4ab
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-03-25 00:04:18 +0530

    Update NumberCheckWithMenu.java

--- NumberCheckWithMenu.java
+++ NumberCheckWithMenu.java
@@ -63,7 +63,7 @@ public class Number
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -71,3 +71,4 @@ public class Number
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
