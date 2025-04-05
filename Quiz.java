93fbab9dcbdb6e3e523d0e6f37efa84f93ee113f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-04-05 09:54:13 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -81,6 +81,9 @@ for (int i=0;i<n;i++){
 
 
 
+
+
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -88,3 +91,4 @@ for (int i=0;i<n;i++){
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
