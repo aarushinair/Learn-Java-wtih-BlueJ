b2c9b6373a442507a9b8dc39d2478ec6fa401bc6
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-04-13 10:56:32 +0530

    Update SeperateDigits.java

--- SeperateDigits.java
+++ SeperateDigits.java
@@ -21,6 +21,17 @@ class separateDigits
 }
 
 
+
+
+
+
+
+
+
+
+
+
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -28,3 +39,4 @@ class separateDigits
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
