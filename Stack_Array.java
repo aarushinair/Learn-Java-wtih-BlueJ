521119b498cc948f50b309c2646f2be1584d7468
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-04-20 08:20:15 +0530

    Update Stack_Array.java

--- Stack_Array.java
+++ Stack_Array.java
@@ -75,7 +75,7 @@ public class Stack_Array
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -83,3 +83,4 @@ public class Stack_Array
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
