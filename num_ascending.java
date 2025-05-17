7e0fb79fe0cb35a7e2e15acd9a15882abeb8bbfc
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-05-17 05:45:55 +0530

    Update num_ascending.java

--- num_ascending.java
+++ num_ascending.java
@@ -33,6 +33,13 @@ public class num_ascending {
 
 
 
+
+
+
+
+
+
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -40,3 +47,4 @@ public class num_ascending {
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
