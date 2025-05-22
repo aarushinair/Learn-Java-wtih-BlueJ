918a7ae067e8bcba665257ccc0bc00759b0184b6
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-05-22 13:25:41 +0530

    Update program.java

--- program.java
+++ program.java
@@ -30,6 +30,13 @@ for(int m=0;m<l;m++){
 
 
 
+
+
+
+
+
+
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -37,3 +44,4 @@ for(int m=0;m<l;m++){
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
