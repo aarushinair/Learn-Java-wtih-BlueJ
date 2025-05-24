7cc01dd8987370df216cd76a1101c0d428341f16
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-05-24 17:11:01 +0530

    Update reverse_number.java

--- reverse_number.java
+++ reverse_number.java
@@ -34,6 +34,17 @@ public class reverse_number
 
 
 
+
+
+
+
+
+
+
+
+
+
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -41,3 +52,4 @@ public class reverse_number
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
